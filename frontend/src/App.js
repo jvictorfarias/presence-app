f96b0ab0ec4ddc0b4f4e6a0616fa2f1911f008d6
jamezaguiar <jamersonaguiar@alu.ufc.br>
2019-11-05 01:29:10 -0300

    frontend: form checks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "./services/api";
 import "./App.css";
 import logo from "./assets/logo.svg";
 
 function App() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (id < 1 || !Number.isInteger(Number(id))) {
+      window.alert("ID inválido! use APENAS números inteiros.");
+    }
+    if (password < 1) {
+      window.alert("Senha em branco!");
+    }
+    if (type < 1) {
+      window.alert("Selecione ALUNO ou PROFESSOR");
+    }
+    console.log(id);
+    console.log(password);
+    console.log(type);
+    // 41:31
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="Presence!" />
+
       <div className="content">
         <p>
           <strong>Gerencie</strong> suas presenças com o{" "}
           <strong>Presence!</strong>
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="text">Identificação</label>
-          <input type="text" id="matr_siape" placeholder="Matrícula/SIAPE" />
+          <input
+            autoFocus
+            type="number"
+            id="id"
+            placeholder="Matrícula/SIAPE"
+            value={id}
+            onChange={event => setId(event.target.value)}
+          />
 
           <label htmlFor="text">Senha</label>
-          <input type="password" id="password" placeholder="Senha" />
+          <input
+            type="password"
+            id="password"
+            placeholder="Senha"
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
 
           <label htmlFor="text">Tipo de Conta</label>
-          <select name="tipo" id="tipo">
+          <select
+            name="tipo"
+            id="type"
+            onChange={event => setType(event.target.value)}
+          >
             <option disabled selected value>
               Aluno / Professor
             </option>
