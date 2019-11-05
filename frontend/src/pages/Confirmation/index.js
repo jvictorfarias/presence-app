1d7cc01735c87fe6cbf4b58d2c01a344138ce610
jamezaguiar <jamersonaguiar@alu.ufc.br>
2019-11-05 18:24:54 -0300

    frontend: new layouts added

--- frontend/src/pages/Confirmation/index.js
+++ frontend/src/pages/Confirmation/index.js
@@ -1,5 +1,43 @@
 import React from "react";
 
+import "./styles.css";
+
 export default function Confirmation() {
-  return <div />;
+  return (
+    <>
+      <div className="contentC">
+        <p>Disciplinas</p>
+
+        <ul className="class-list">
+          <li>
+            <header>Redes de Comunicação Móveis</header>
+            <strong>Sala 3 - Bloco 1</strong>
+            <span>
+              QUA - 20:00 às 22:00
+              <br />
+              SEX - 18:00 às 20:00
+            </span>
+
+            <button className="btn" type="submit">
+              CONFIRMAR PRESENÇA
+            </button>
+          </li>
+
+          <li>
+            <header>Sistemas Distribuídos</header>
+            <strong>Sala 3 - Bloco 1</strong>
+            <span>
+              SEG - 20:00 às 22:00
+              <br />
+              TER - 18:00 às 20:00
+            </span>
+
+            <button className="btn" type="submit">
+              CONFIRMAR PRESENÇA
+            </button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
